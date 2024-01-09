7e1e0d7e5534fc293094e81c411c84d1be21435d
Hrithik Joshi <40289232+hrithik01@users.noreply.github.com>
2024-01-10 02:20:17 +0530

    Handling errors

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,8 +1,8 @@
 'use client'
-import { TextField, TextArea, Button } from '@radix-ui/themes'
+import { TextField, TextArea, Button, Callout } from '@radix-ui/themes'
 import SimpleMDE from "react-simplemde-editor"
 import "easymde/dist/easymde.min.css"
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 import { useForm, Controller } from 'react-hook-form'
 import { useRouter } from 'next/navigation'
@@ -15,12 +15,23 @@ interface IssueForm {
 const NewIssuePage = () => {
   const router = useRouter()
   const { register, control, handleSubmit } = useForm<IssueForm>()
+  const [error, setError] = useState('')
   return (
+  <div className='max-w-xl'> 
+    { error && (
+      <Callout.Root color="red" className='mb-4'>
+        <Callout.Text>{error}</Callout.Text>
+      </Callout.Root>
+    )}
     <form 
-    className='max-w-xl space-y-3' 
+    className='space-y-3' 
     onSubmit={handleSubmit(async (data) => {
-      await axios.post('/api/issues', data)
-      router.push('/issues')
+      try {
+        await axios.post('/api/issues', data)
+        router.push('/issues')
+      } catch (error) {
+        setError('An error occurred while creating the issue')
+      }
     })}>
         <TextField.Root>
             <TextField.Input placeholder='Title' {...register('title')}/>
@@ -32,6 +43,7 @@ const NewIssuePage = () => {
         />
         <Button>Submit New Issue</Button>
     </form>
+  </div>
   )
 }
 
